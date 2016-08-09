18f205e66dc7fd5636cfd84f3da08018feb286de
Vladislavs Marisevs <vxm93@eis.equifax.com>
2016-08-09 14:33:23 +0100

    6. Routing

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -3,6 +3,10 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 @Injectable()
 export class HeroService {
+  getHero(id: number) {
+    return this.getHeroes()
+    .then(heroes => heroes.find(hero => hero.id === id));
+  }
   getHeroes() {
     return Promise.resolve(HEROES);
   }
